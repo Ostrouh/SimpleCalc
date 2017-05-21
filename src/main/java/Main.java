cf6393c9ae2573ea7ce18712b219ba850099ae34
Eugene <ostrouh.e@gmail.com>
2017-05-21 19:17:55 +0300

    Added View and Controller. Changed  Main.java for work with javafX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,46 @@
-import Model.Calc;
-import Model.ReversePolishNotation;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Created by ostrouh on 15.05.2017.
  */
-public class Main {
+public class Main extends Application {
     private static final Logger log = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
-        String input;
+        launch(args);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        while (!(input = reader.readLine()).equals("exit")) {
+//            log.info("Expression recieved");
+//            String output = null;
+//            try {
+//                output = ReversePolishNotation.createOutput(input);
+//                double result = new Calc().getResult(output);
+//                log.info("Result recieved");
+//                System.out.println("Result is: " + result);
+//            } catch (IllegalArgumentException e) {
+//                log.error("Illegal expression: " + e.getMessage(), e);
+//                System.out.println("Illegal expression: " + e.getMessage());
+//            }
+//        }
+//        reader.close();
+//        log.info("Конец работы");
+//        reader.close();
+//        log.info("End of work");
+    }
+    public void start(Stage primaryStage) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        while (!(input = reader.readLine()).equals("exit")) {
-            log.info("Expression recieved");
-            String output = null;
-            try {
-                output = ReversePolishNotation.createOutput(input);
-                double result = new Calc().getResult(output);
-                log.info("Result recieved");
-                System.out.println("Result is: " + result);
-            } catch (IllegalArgumentException e) {
-                log.error("Illegal expression: " + e.getMessage(), e);
-                System.out.println("Illegal expression: " + e.getMessage());
-            }
-        }
-        reader.close();
-        log.info("Конец работы");
-        reader.close();
-        log.info("End of work");
+        Parent root = FXMLLoader.load(getClass().getResource("View/Main.fxml"));
+        primaryStage.setTitle("SimpleCalc");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
+
