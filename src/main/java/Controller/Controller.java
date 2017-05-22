d42e33a9e41ffed450d121d68e94695a9d9cb361
Eugene <ostrouh.e@gmail.com>
2017-05-22 18:18:06 +0300

    without contrller work

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,128 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by ostrouh on 21.05.2017.
+ */
+public class Controller {
+    @FXML
+    private TextField tf;
+    @FXML
+    private Button buttonC;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button button0;
+    @FXML
+    private Button buttonPlus;
+    @FXML
+    private Button buttonMinus;
+    @FXML
+    private Button buttonMultiply;
+    @FXML
+    private Button buttonDivide;
+    @FXML
+    private Button buttonDot;
+    @FXML
+    private Button buttonToDegrees;
+    @FXML
+    private Button buttonEqual;
+
+    @FXML
+    public void onClickButtonC(){
+
+    }
+    @FXML
+    public void onClickButton1(){
+
+    }
+    @FXML
+    public void onClickButton2(){
+
+    }
+    @FXML
+    public void onClickButton3(){
+
+    }
+    @FXML
+    public void onClickButton4(){
+
+    }
+    @FXML
+    public void onClickButton5(){
+
+    }
+    @FXML
+    public void onClickButton6(){
+
+    }
+    @FXML
+    public void onClickButton7(){
+
+    }
+    @FXML
+    public void onClickButton8(){
+
+    }
+    @FXML
+    public void onClickButton9(){
+
+    }
+    @FXML
+    public void onClickButton0(){
+
+    }
+    @FXML
+    public void onClickButtonPlus(){
+
+    }
+    @FXML
+    public void onClickButtonMinus(){
+
+    }
+    @FXML
+    public void onClickButtonMultiply(){
+
+    }
+    @FXML
+    public void onClickButtonDivide(){
+
+    }
+    @FXML
+    public void onClickButtonToDegrees(){
+
+    }
+    @FXML
+    public void onClickButtonEqual(){
+
+    }
+    @FXML
+    public void onClickButtonDot(){
+
+    }
+    @FXML
+    public void onClickButtonBS(){
+
+    }
+
+
+}
